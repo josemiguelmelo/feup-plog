a14b497528d907b95b2e6e7ab8b1ffd9c564ee83
Jose Miguel Melo <melo199435@gmail.com>
2014-11-07 19:50:12 +0000

    move almost complete

--- game.pl
+++ game.pl
@@ -12,6 +12,53 @@ nth0(N,[_|T],Item) :-       % Clause added KJ 4-5-87 to allow mode
     N is M + 1.
 
 
+player(a1).
+player(b1).
+next_player(a1, b1).
+next_player(b1, a1).
+
+piece(a1).
+piece(a2).
+piece(a3).
+piece(b1).
+piece(b2).
+piece(b3).
+piece(p1).
+piece(p2).
+piece(vv).
+
+next_piece(a1, p1, a2).
+next_piece(a2, p1, a3).
+next_piece(b1, p1, b2).
+next_piece(b2, p1, b3).
+next_piece(a1, vv, a1).
+next_piece(b1, vv, b1).
+
+
+remove_piece(a1, a1, vv).
+remove_piece(a1, a2, p1).
+remove_piece(a1, a3, a3).
+remove_piece(a1, vv, vv).
+remove_piece(a1, p1, p1).
+remove_piece(a1, p2, p2).
+
+remove_piece(a1, b1, b1).
+remove_piece(a1, b2, b2).
+remove_piece(a1, b3, b3).
+
+
+
+
+remove_piece(b1, b1, vv).
+remove_piece(b1, b2, p1).
+remove_piece(b1, b3, b3).
+remove_piece(b1, vv, vv).
+remove_piece(b1, p1, p1).
+remove_piece(b1, p2, p2).
+
+remove_piece(b1, a1, a1).
+remove_piece(b1, a2, a2).
+remove_piece(b1, a3, a3).
 
 
 %print board
@@ -58,24 +105,54 @@ checkPosition(Board, X, Y):-
 	(Element == vv ; Element == p1).
 
 
-move([], Xinitial,Yinitial, Xfinal,Yfinal, BoardAux, FinalBoard, Counter, Player):-
-	append(BoardAux,[], FinalBoard).
-% falta ver para que valor a alterar
-move([Line|Tail], Xinitial,Yinitial, Xfinal,Yfinal, BoardAux, FinalBoard, Counter, Player):-
-	Xinitial > 0 , Yinitial > 0, Xfinal > 0 , Yfinal > 0,
-	Xinitial < 6 , Yinitial < 6, Xfinal < 6 , Yfinal < 6,
-	(Counter==Xinitial -> 
-			
-		replace(Line, Yinitial, Player, FinalLine),
-		C is Counter+1,
-		append(BoardAux,[FinalLine], Aux),
-		move(Tail, Xinitial,Yinitial, Xfinal,Yfinal, Aux, FinalBoard, C, Player);
+% move line
+move_line([], Y, BoardAux, Player, FinalAux):-
+	append( BoardAux, [], FinalAux).
+
+move_line([P|Line], 0, BoardAux, Player, FinalAux):-
+	next_piece(Player, P , NextPiece),
+	append(BoardAux, [NextPiece], Aux),
+	Yaux is -1,
+	move_line(Line, Yaux, Aux, Player, FinalAux).
+
+move_line([P|Line], Y, BoardAux, Player, FinalAux):-
+	append(BoardAux, [P], Aux),
+	Yaux is Y-1,
+	move_line(Line, Yaux, Aux, Player, FinalAux).
+
+% move pawn to (X, Y)
+move([], X, Y, BoardAux, FinalBoard, Player):-
+	append([], BoardAux, FinalBoard).
+
+move([Line|Tail], 0, Y, BoardAux, FinalBoard, Player):-
+	move_line(Line, Y, [], Player, FinalLine),
+	append(BoardAux, [FinalLine], Aux),
+	Xaux is -1,
+	move(Tail, Xaux, Y, Aux, FinalBoard, Player).
+
+move([Line|Tail], X, Y, BoardAux, FinalBoard, Player):-
+	append(BoardAux, [Line], Aux),
+	Xaux is X-1,
+	move(Tail, Xaux, Y, Aux, FinalBoard, Player).
 
 
-		append(BoardAux,[Line], Aux), 
-		C is Counter+1,
-		move(Tail, Xinitial,Yinitial, Xfinal,Yfinal, Aux, FinalBoard, C, Player)
-	).
+% remove spawn from board. 
+remove_spawn_line([], BoardAux, Player, FinalLine):-
+	append([], BoardAux, FinalLine).
+
+remove_spawn_line([P|Line], BoardAux,Player, FinalLine):-
+	remove_piece(Player, P, NewPiece),
+	append(BoardAux, [NewPiece], Aux),
+	remove_spawn_line(Line, Aux, Player, FinalLine).
+
+
+remove_spawn([], BoardAux, FinalBoard, Player):-
+	append([], BoardAux, FinalBoard).
+
+remove_spawn([Line|Tail], BoardAux, FinalBoard, Player):-
+	remove_spawn_line(Line, [], Player, FinalLine),
+	append(BoardAux, [FinalLine], Aux),
+	remove_spawn(Tail, Aux, FinalBoard, Player).
 
 % return the value to insert at position where to move to
 nextPositionValue(Board, X, Y, Value, Pawn):-
@@ -84,28 +161,25 @@ nextPositionValue(Board, X, Y, Value, Pawn):-
 
 % read from terminal position where to move to
 choose_move_player(Board, Player, XDest, YDest) :-
-	repeat,
     write('Move Dest Line (number): '),
     read(XDest), skip_line,
     write('Move Dest Column (letter): '),
     read(YDest), skip_line,
     checkPosition(Board, XDest, YDest).
 
-player(a1).
-player(b1).
-next_player(player1, player2).
-next_player(player2, player1).
 
 % game auxiliar function, no init
 game_aux(Board, Player):-
 	printBoard(Board),nl,nl,
 	write(Player) , write(' '), write('turn. '), nl,
 	choose_move_player(Board, Player, X, Y),
-	move(Board, X,Y, X,Y, [], FinalBoard, 0,Player),
+	remove_spawn(Board, [], FinalBoard, Player),
+	write(FinalBoard),nl,
+	move(FinalBoard, X,Y, [], FinalBoard1, Player),
 	next_player(Player, NextPlayer),
-	game_aux(FinalBoard, NextPlayer).
+	game_aux(FinalBoard1, NextPlayer).
 
 % game function with init
 game(Board):-
 	initBoard(Board),
-	game_aux(Board, player1).
+	game_aux(Board, a1).
--- printBoard.pl
+++ printBoard.pl
@@ -16,3 +16,25 @@ printLine([X | B]) :-
 	printLine(B).
 
 
+
+
+
+
+move([], Xinitial,Yinitial, Xfinal,Yfinal, BoardAux, FinalBoard, Counter, Player):-
+	append(BoardAux,[], FinalBoard).
+
+% falta ver para que valor a alterar
+move([Line|Tail], Xinitial,Yinitial, Xfinal,Yfinal, BoardAux, FinalBoard, Counter, Player):-
+	Xinitial > 0 , Yinitial > 0, Xfinal > 0 , Yfinal > 0,
+	Xinitial < 6 , Yinitial < 6, Xfinal < 6 , Yfinal < 6,
+	(Counter==Xinitial -> 
+		replace(Line, Yinitial, Player, FinalLine),
+		C is Counter+1,
+		append(BoardAux,[FinalLine], Aux),
+		move(Tail, Xinitial,Yinitial, Xfinal,Yfinal, Aux, FinalBoard, C, Player);
+
+
+		append(BoardAux,[Line], Aux), 
+		C is Counter+1,
+		move(Tail, Xinitial,Yinitial, Xfinal,Yfinal, Aux, FinalBoard, C, Player)
+	).
